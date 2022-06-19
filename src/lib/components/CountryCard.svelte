d12e0424666aff90b6b5ded739245f395d4b9020
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2022-06-19 13:40:44 +0300

    transition change

--- src/lib/components/CountryCard.svelte
+++ src/lib/components/CountryCard.svelte
@@ -7,7 +7,7 @@
     export let onClicked;
 </script>
 
-<div on:click={onClicked} class="flex flex-col rounded-md shadow-md bg-white dark:bg-dark-blue dark:text-white pb-11 hover:scale-105 hover:duration-700 transition-colors duration-200 cursor-pointer">
+<div on:click={onClicked} class="flex flex-col rounded-md shadow-md bg-white dark:bg-dark-blue dark:text-white pb-11 hover:scale-105 hover:duration-700 transition duration-200 cursor-pointer">
     <div class="h-[200px] w-full overflow-hidden box-border">
         <img class="  w-full h-full max-w-full rounded-t-md object-cover" alt="flag" src={countryImageSrc} />
     </div>
